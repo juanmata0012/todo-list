14092164e396226e190170ce852ed960ca0ae26c
Juan Mata <juanmata@Juans-MacBook-Pro.local>
2018-07-11 21:43:45 -0500

    add helpers

--- routes/todos.js
+++ routes/todos.js
@@ -1,47 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var db = require("../models");
+var helpers = require('../helpers/todos');
+
+router.route('/')
+  .get(helpers.getTodos)
+  .post(helpers.createTodo)
+
+router.route('/:todoId')
+  .get(helpers.getTodo)
+  .put(helpers.updateTodo)
+  .delete(helpers.deleteTodo)
+
+module.exports = router;
+
+/*
+
+All of these routes were originally in this file and moved to the helpers folder to clean up our code
+
 
 router.get('/', );
 
-router.post('/', function(req, res){
-  db.Todo.create(req.body)
-  .then(function(newTodo){
-    res.json(newTodo)
-  })
-  .catch(function(err){
-    res.send(err);
-  })
-})
+router.post('/', )
 
-router.get('/:todoId', function(req, res){
-  db.Todo.findById(req.params.todoId)
-  .then(function(foundTodo){
-    res.json(foundTodo)
-  })
-  .catch(function(err){
-    res.send(err);
-  })
-})
+router.get('/:todoId', )
 
-router.put('/:todoId', function(req, res){
-  db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
-  .then(function(todo){
-    res.json(todo);
-  })
-  .catch(function(err){
-    res.send(err);
-  })
-})
+router.put('/:todoId', )
 
-router.delete('/:todoId', function(req, res){
-  db.Todo.remove({_id: req.params.todoId})
-  .then(function(){
-    res.json({message: 'We deleted it!'});
-  })
-  .catch(function(err){
-    res.send(err);
-  })
-})
+router.delete('/:todoId', )
 
-module.exports = router;
+*/
