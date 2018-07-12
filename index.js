1d8820a8a62fe647f37d89a40c56fa0c6b1ee930
Juan Mata <juanmata@Juans-MacBook-Pro.local>
2018-07-12 00:44:14 -0500

    todo list done

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ var todoRoutes = require('./routes/todos');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res){
-  res.send({message: "Hello from the ROOT Route"})
+  res.sendFile("index.html")
 })
 
 app.use('/api/todos', todoRoutes);
